fb80edf0fde6b7c22e01904a1c68f905f749a4d9
雪あすか <tt@kmycode.net>
2019-01-03 19:57:22 +0900

    フッタの表記を修正

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -4,10 +4,11 @@
       <hr>
       三国志NET Ver2.7 <a href="http://www3.to/maccyu2/" target="_top">maccyu</a><br>
       三国志NET KMY Version (since 2006.8.21)<br>
-      Administered by <a href="https://twitter.com/askyq">雪あすか</a> (※成人向けコンテンツ注意)
+      Administered by 雪あすか <a href="https://github.com/kmycode">GitHub</a> / <a href="https://twitter.com/askyq">Twitter</a> (※成人向けコンテンツ注意)
       <div id="footer-last-mes">
         ※本ゲームプログラムは、原作Ver2.7ならびに三国志NET KMY Version 3／7を参考に、一から再構成したものです<br>
-        Windows 10とGoogle Chrome、Microsoft Edgeで動作を確認しています
+        Google Chrome(Win, Mac)、Safari(Mac) で動作を確認しております<br>
+        Internet Explorer、Microsoft Edgeでも、操作に不都合がないことは確認していますが、表示の崩れまでは対応していません。<del>潰れて</del>
       </div>
     </div>
   </footer>
