b19a25b09c45d96533823477fc9826ae05113ee0
雪あすか <tt@kmycode.net>
2019-02-17 17:33:04 +0900

    ログイン画面の画像を設定

--- src/components/pages/LoginPage.vue
+++ src/components/pages/LoginPage.vue
@@ -104,7 +104,9 @@ export default class LoginPage extends Vue {
 
   width: 100vw;
   height: 100vh;
-  background: #99ccff;
+  background: #99ccff url('../../assets/images/login-background.png') no-repeat;
+  background-size: cover;
+  background-position: center;
   display: flex;
   flex-direction: column;
   justify-content: center;
