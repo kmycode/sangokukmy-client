38a83f06e98961ed7fa14b09ddf3f81e634f2375
雪あすか <tt@kmycode.net>
2019-04-12 21:27:23 +0900

    武将所在都市のストリーミング

--- src/models/status/statusstore.ts
+++ src/models/status/statusstore.ts
@@ -19,10 +19,15 @@ export default class StatusStore {
   public countries: api.Country[] = [];
   public country: api.Country = api.Country.default;  // 自分の所属しない国が入る場合がある
   public policies: api.CountryPolicy[] = [];
+  public wars: api.CountryWar[] = [];
+  public alliances: api.CountryAlliance[] = [];
   public scouters: api.CountryScouter[] = [];
   public towns: api.TownBase[] = [];
   public town: api.TownBase = new api.Town(-1);           // 自分の所在しない都市が入る場合がある
+  public townWars: api.TownWar[] = [];
   public character: api.Character = new api.Character(-1);  // 常に自分が入る
+  public characters: api.Character[] = [];
+  public defenders: api.TownDefender[] = [];
   public units: api.Unit[] = [];
   public reinforcements: api.Reinforcement[] = [];
   public soldierTypes: api.CharacterSoldierType[] = [];
