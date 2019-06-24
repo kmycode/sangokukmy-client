64f86e8f33613e131f539c97a4510bee95f373e1
雪あすか <tt@kmycode.net>
2019-06-25 07:54:13 +0900

    メニューに説明書へのリンクを追加

--- src/components/pages/StatusPage.vue
+++ src/components/pages/StatusPage.vue
@@ -207,6 +207,8 @@
                 <a class="dropdown-item" href="#" @click.prevent.stop="selectedActionTab = 3; selectedActionTabSubPanel = 6; isOpenRightSidePopupMenu = false">模擬戦闘</a>
                 <a class="dropdown-item" href="#" @click.prevent.stop="selectedActionTab = 3; selectedActionTabSubPanel = 7; isOpenRightSidePopupMenu = false">模擬米施し</a>
                 <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="https://sangoku-doc.kmycode.net/" target="_blank">説明書</a>
+                <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" @click.prevent.stop="model.logout(); $router.push('home');">ログアウト</a>
               </div>
             </li>
