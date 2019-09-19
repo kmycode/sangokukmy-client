d8c9c705c74d2b9e631b1f43a9390bde9c2e3216
雪あすか <tt@kmycode.net>
2019-09-19 18:05:17 +0900

    技能のID値がずれている問題を修正

--- src/components/parts/status/CommandList.vue
+++ src/components/parts/status/CommandList.vue
@@ -94,7 +94,7 @@
         <button v-if="list.canInputIncreasePeople" type="button" class="btn btn-light" :disabled="!list.inputer.canInput" @click="list.inputer.inputCommand(58)">農民呼寄</button>
         <button v-if="list.canInputDecreasePeople" type="button" class="btn btn-light" :disabled="!list.inputer.canInput" @click="list.inputer.inputCommand(59)">農民避難</button>
         <button v-if="list.canInputSoldierTrainingAll" type="button" class="btn btn-light" :disabled="!list.inputer.canInput" @click="list.inputer.inputCommand(60)">合同訓練</button>
-        <button v-if="list.canInputSpy" type="button" class="btn btn-light" :disabled="!list.inputer.canInput" @click="list.inputer.inputCommand(61)">偵察</button>
+        <button v-if="list.canInputSpy" type="button" class="btn btn-light" :disabled="!list.inputer.canInput" @click="list.inputer.inputMoveCommand(61)">偵察</button>
         <button v-if="canCommandComment" type="button" class="btn btn-primary" :disabled="!list.inputer.canInput" @click="$emit('open', 'command-comment')">コメント</button>
       </div>
       <div class="loading" v-show="list.inputer.isInputing"><div class="loading-icon"></div></div>
--- src/models/status/commandlist.ts
+++ src/models/status/commandlist.ts
@@ -64,39 +64,39 @@ export default class CommandList {
   public get canInputGenerateItem2(): boolean {
     // 胡人交易
     const skills = this.store.skills.filter((s) => s.characterId === this.store.character.id).map((s) => s.type);
-    return skills.some((s) => s === 25);
+    return skills.some((s) => s === 30);
   }
 
   public get canInputGenerateItem3(): boolean {
     // 精鋭検査
     const skills = this.store.skills.filter((s) => s.characterId === this.store.character.id).map((s) => s.type);
-    return skills.some((s) => s === 27);
+    return skills.some((s) => s === 32);
   }
 
   public get canInputGenerateItem4(): boolean {
     // 書物執筆
     const skills = this.store.skills.filter((s) => s.characterId === this.store.character.id).map((s) => s.type);
-    return skills.some((s) => s === 34 || s === 38);
+    return skills.some((s) => s === 39 || s === 43);
   }
 
   public get canInputIncreasePeople(): boolean {
     const skills = this.store.skills.filter((s) => s.characterId === this.store.character.id).map((s) => s.type);
-    return skills.some((s) => s === 24 || s === 29);
+    return skills.some((s) => s === 29 || s === 34);
   }
 
   public get canInputDecreasePeople(): boolean {
     const skills = this.store.skills.filter((s) => s.characterId === this.store.character.id).map((s) => s.type);
-    return skills.some((s) => s === 24 || s === 29);
+    return skills.some((s) => s === 29 || s === 34);
   }
 
   public get canInputSoldierTrainingAll(): boolean {
     const skills = this.store.skills.filter((s) => s.characterId === this.store.character.id).map((s) => s.type);
-    return skills.some((s) => s === 32);
+    return skills.some((s) => s === 37);
   }
 
   public get canInputSpy(): boolean {
     const skills = this.store.skills.filter((s) => s.characterId === this.store.character.id).map((s) => s.type);
-    return skills.some((s) => s === 40);
+    return skills.some((s) => s === 45);
   }
 
   public get restTurns(): number {
