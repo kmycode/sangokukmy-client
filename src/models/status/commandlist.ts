c0d8cab3e51e9cc7275e04660626300e2f8fa863
雪あすか <tt@kmycode.net>
2019-09-27 07:50:21 +0900

    武家・官吏Lv.2で都市巡回を入力できない問題を修正

--- src/models/status/commandlist.ts
+++ src/models/status/commandlist.ts
@@ -47,7 +47,7 @@ export default class CommandList {
 
   public get canInputTownPatrol(): boolean {
     const skills = this.store.skills.filter((s) => s.characterId === this.store.character.id).map((s) => s.type);
-    return skills.some((s) => s === 5 || s === 10);
+    return skills.some((s) => s === 2 || s === 7);
   }
 
   public get canInputTownInvent(): boolean {
