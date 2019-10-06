792a73c7a5cf060eba8afb75ae7631cb37a77759
雪あすか <tt@kmycode.net>
2019-10-06 21:11:25 +0900

    梓琴兵が青洲兵と混合される問題を修正

--- src/common/definitions.ts
+++ src/common/definitions.ts
@@ -109,7 +109,7 @@ export const SOLDIER_TYPES: SoldierType[] = [
   new SoldierType(23, 1, '梓叡兵', 3, 200, '0', '0', '武力の代わりに知力が使用される雑兵', undefined, undefined, undefined, [48, 10]),
   new SoldierType(24, 1, '梓馬兵', 18, 900, '60', '40', '基礎能力として武力の代わりに知力を用いた兵種。武官の重騎兵と同等の補正を持つ'),
   new SoldierType(25, 3, '義勇兵', 10, 200, '0', '0', '正義を振り翳せば従ってくれるちょろい愚民たち。基礎能力として人望を用いる', 30),
-  new SoldierType(26, 1, '梓琴兵', 22, 900, '90', '40', '基礎能力として武力の代わりに知力を用いた兵種。武官の連弩兵と同等の補正を持つ', undefined, undefined, undefined, 50),
+  new SoldierType(29, 1, '梓琴兵', 22, 900, '90', '40', '基礎能力として武力の代わりに知力を用いた兵種。武官の連弩兵と同等の補正を持つ', undefined, undefined, undefined, 50),
   new SoldierType(100, 0, '守兵A', 32767, 32767, '0', '0', ''),
   new SoldierType(101, 0, '守兵B', 32767, 32767, '0', '0', ''),
   new SoldierType(102, 0, '守兵C', 32767, 32767, '0', '0', ''),
