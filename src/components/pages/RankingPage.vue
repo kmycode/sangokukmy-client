956d5a55693dcca42a701b2d05a2ccd6b80cad0f
雪あすか <tt@kmycode.net>
2020-04-02 10:13:41 +0900

    戦闘ランキングにAI武将を追加

--- src/components/pages/RankingPage.vue
+++ src/components/pages/RankingPage.vue
@@ -102,12 +102,12 @@ export default class RankingPage extends Vue {
     this.rankings.push(new RankingData('人望', 'popularity', charas));
     this.rankings.push(new RankingData('階級', 'classValue', charas));
     this.rankings.push(new RankingData('資産', 'money', charas));
-    this.rankings.push(new RankingData('戦闘勝利数', 'battleWonCount', charas));
-    this.rankings.push(new RankingData('戦闘敗北数', 'battleLostCount', charas));
-    this.rankings.push(new RankingData('勝率', 'winRate', charas));
-    this.rankings.push(new RankingData('城壁破壊量', 'battleBrokeWallSize', charas));
-    this.rankings.push(new RankingData('支配数', 'battleDominateCount', charas));
-    this.rankings.push(new RankingData('連戦数', 'battleContinuousCount', charas));
+    this.rankings.push(new RankingData('戦闘勝利数', 'battleWonCount', charas, true));
+    this.rankings.push(new RankingData('戦闘敗北数', 'battleLostCount', charas, true));
+    this.rankings.push(new RankingData('勝率', 'winRate', charas, true));
+    this.rankings.push(new RankingData('城壁破壊量', 'battleBrokeWallSize', charas, true));
+    this.rankings.push(new RankingData('支配数', 'battleDominateCount', charas, true));
+    this.rankings.push(new RankingData('連戦数', 'battleContinuousCount', charas, true));
   }
 
   private getCountryColorId(countryId: number): number {
