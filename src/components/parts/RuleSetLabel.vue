48f13fa4af87654a18bc3ddbb7e6ab2fdba5b0fa
KMY <tt@kmycode.net>
2020-09-01 13:28:35 +0900

    玉璽ルールの追加

--- src/components/parts/RuleSetLabel.vue
+++ src/components/parts/RuleSetLabel.vue
@@ -4,6 +4,7 @@
     <span v-else-if="ruleSet === 1" style="color:purple;font-weight:bold">放浪ルール</span>
     <span v-else-if="ruleSet === 2" style="color:black;font-weight:bold">原理ルール</span>
     <span v-else-if="ruleSet === 3" style="color:#800;font-weight:bold">全国戦争ルール</span>
+    <span v-else-if="ruleSet === 4" style="color:#990;font-weight:bold">玉璽ルール</span>
     <span v-else>不明のルール</span>
   </span>
 </template>
