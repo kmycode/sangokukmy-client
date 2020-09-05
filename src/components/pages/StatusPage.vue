111fc2d0d5bec036ef6ff2230b05622f6ba51b7a
KMY <tt@kmycode.net>
2020-09-05 20:24:22 +0900

    都市購入に年制限を追加

--- src/components/pages/StatusPage.vue
+++ src/components/pages/StatusPage.vue
@@ -1632,7 +1632,7 @@
             <h4>現在の政策ポイント: {{ model.characterCountry.policyPoint }}</h4>
             <div class="alert alert-danger" v-show="model.townBuyCost > model.characterCountry.policyPoint">購入に必要な政策ポイントが足りません</div>
             <div class="alert alert-danger" v-show="!model.town.countryId">無所属都市は購入できません</div>
-            <div class="alert alert-danger" v-show="model.gameDate.year < 36">戦闘解除前は購入できません</div>
+            <div class="alert alert-danger" v-show="model.gameDate.year < 48">48 年より前は購入できません</div>
             <div class="alert alert-info" v-show="model.townBuyCost <= model.characterCountry.policyPoint">都市を購入可能です</div>
           </div>
           <div class="loading" v-show="model.isUpdatingTownBuyCost"><div class="loading-icon"></div></div>
@@ -1642,7 +1642,7 @@
             <button class="btn btn-light" @click="isOpenBuyTownDialog = false">閉じる</button>
           </div>
           <div class="right-side">
-            <button class="btn btn-primary" v-show="!model.isUpdatingTownBuyCost && model.town.countryId && model.canDiplomacy && model.gameDate.year >= 36 && model.characterCountry.policyPoint >= model.townBuyCost" @click="model.buyTown(); isOpenBuyTownDialog = false">承認</button>
+            <button class="btn btn-primary" v-show="!model.isUpdatingTownBuyCost && model.town.countryId && model.canDiplomacy && model.gameDate.year >= 48 && model.characterCountry.policyPoint >= model.townBuyCost" @click="model.buyTown(); isOpenBuyTownDialog = false">承認</button>
           </div>
         </div>
       </div>
